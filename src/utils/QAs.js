bb6ed66cc7ccfe117cb819fc62222f88a4f1466a
oscarcast22 <oscar_cm96@outlook.com>
2026-08-20 11:43:48 -0600

    actualizar faqs y dependencias, cambiar gestor de paquetes por pnpm

--- src/utils/QAs.js
+++ src/utils/QAs.js
@@ -1,99 +1,99 @@
 const QAs = [
     [
-        "1.- ¿QUE HORARIOS SE MANEJAN EN MEDICINA VIRTUAL? SI NO ME PUEDO CONECTAR EN ESE HORARIO DE CLASES.",
-        "Los horarios se manejan para clases en vivo entre las 8 y 14 hrs. de lunes a viernes, sin embargo, existe flexibilidad ya que las clases se quedan grabadas en plataforma, dando oportunidad de estudio del módulo semanal realizar actividades académicas y de autoestudio en plataforma hasta el domingo de esa semana a las 23:59 hrs."
+        "1.- ¿QUÉ HORARIOS SE MANEJAN EN MEDICINA VIRTUAL? SI NO ME PUEDO CONECTAR EN ESE HORARIO DE CLASES.",
+        "Los horarios se manejan para clases en vivo entre las 08:00 y las 19:00 hrs. de lunes a viernes, y los sábados de 09:00 a 14:00 hrs. Sin embargo, existe flexibilidad, ya que las clases se quedan grabadas en plataforma, dando oportunidad de estudio del módulo semanal para realizar actividades académicas y de autoestudio en plataforma hasta el domingo de esa semana a las 23:59 hrs."
     ],
     [
-        "2.- ¿CUAL ES EL PROCESO PARA LLEVAR LAS CLASES?",
-        "Una vez inscrito, debes contactar a la Dirección de la Escuela con el Dr. Julio Díaz o el Dr. Alberto Díaz, a fin de que se te de información detallada sobre las asignaturas que vas a cursar, el grupo correspondiente y el calendario de actividades en plataforma, es indispensable de la misma forma que se te inscriba al proceso de inducción a la licenciatura, por el momento no contamos con examen de admisión, sino con proceso de inducción, el cual es obligatorio."
+        "2.- ¿CUÁL ES EL PROCESO PARA LLEVAR LAS CLASES?",
+        "Una vez inscrito, debes contactar a Control Escolar, a fin de que se te dé información detallada sobre las asignaturas que vas a cursar, el grupo correspondiente y el calendario de actividades en plataforma. Es indispensable, de la misma forma, que se te inscriba al proceso de inducción a la licenciatura. Por el momento no contamos con examen de admisión, sino con un proceso de inducción, el cual es obligatorio."
     ],
     [
         "3.- ¿AFECTA MI CALIFICACIÓN EL NO PODERME CONECTAR EN EL HORARIO?",
-        "No afecta siempre y cuando se atiendan las indicaciones de participar en los foros correspondientes y se atienda la videograbación de clase, así como la participación en actividades de práctica y esclarecimiento de dudas."
+        "No afecta, siempre y cuando se atiendan las indicaciones de participar en los foros correspondientes y se atienda la videograbación de la clase, así como la participación en actividades de práctica y esclarecimiento de dudas."
     ],
     [
-        "4.- ¿LAS CLASES SON ON-LINE, ¿SON GRABADAS, SON VIDEOTUTORIALES Y LOS PUEDO VER EN CUALQUIER MOMENTO SIN CONECTARME EN EL HORARIO?",
-        "Si, se colocan en plataforma y se encuentran activas durante el tiempo de desarrollo del módulo 1 semana."
+        "4.- ¿LAS CLASES SE IMPARTEN COMPLETAMENTE EN LÍNEA? ¿LAS SESIONES SON EN VIVO Y REQUIEREN CONECTARSE EN UN HORARIO ESPECÍFICO, O QUEDAN GRABADAS Y/O SE OFRECEN MEDIANTE VIDEOTUTORIALES QUE PUEDEN CONSULTARSE EN CUALQUIER MOMENTO?",
+        "Sí, se colocan en plataforma y se encuentran activas durante el tiempo de desarrollo de cada módulo, respetando la semana correspondiente a evaluar. Sin embargo, se solicita que al menos se cumpla con un 66% de asistencia a clases."
     ],
     [
-        "5.- ¿COMO SE LLEVAN LAS CLASES VIRTUALES DE MEDICINA?",
-        "Se llevan a cabo de manera presencial o video grabadas siempre apoyados con un asesor académico. Para el proceso de desarrollo de actividades académicas, durante la semana se generan además actividades de práctica obligatoria con tele simulación con la más alta tecnología y foros de dudas, discusión, exámenes y toda una gama de actividades académicas semanales."
+        "5.- ¿CÓMO SE LLEVAN LAS CLASES VIRTUALES DE MEDICINA?",
+        "Se llevan a cabo de manera presencial o videograbada, siempre con el apoyo de un asesor académico. Para el proceso de desarrollo de actividades académicas, durante la semana se generan además actividades de práctica obligatoria con telesimulación con la más alta tecnología, así como foros de dudas, discusión, exámenes y toda una gama de actividades académicas semanales."
     ],
     [
-        "6.- ¿QUE PLATAFORMA SE VA A USAR PARA LAS CLASES?",
-        "Se utiliza la plataforma de la Universidad Campus Virtual con apoyo de plataformas educativas propias de la UAD, además de las videoconferencias por zoom, y otros recursos de realidad virtual, con plataformas como team viewer, etc."
+        "6.- ¿QUÉ PLATAFORMA SE VA A USAR PARA LAS CLASES?",
+        "Se utiliza la plataforma de la Universidad, Campus Virtual, con apoyo de plataformas educativas propias de la UAD, además de videoconferencias por Google Meet, usando un correo institucional asignado por la Universidad."
     ],
     [
-        "7.- ¿QUE HERRAMIENTAS TECNOLÓGICAS Y APLICACIONES SE UTILIZAN PARA LAS CLASES?",
+        "7.- ¿QUÉ HERRAMIENTAS TECNOLÓGICAS Y APLICACIONES SE UTILIZAN PARA LAS CLASES?",
         "Se utiliza la más alta tecnología en educación médica con simuladores de alta fidelidad, así como recursos de realidad virtual, evaluación entre pares, simulación entre pares, pacientes estandarizados, etc."
     ],
     [
-        "8.- ¿COMO SON LAS CLASES PRACTICAS?",
-        "A partir de 5 semestre se desarrollan actividades prácticas en dos escenarios distintos, uno mediante la simulación a distancia con la más alta tecnología como lo desarrollan las mejores universidades del mundo, y además se lleva a cabo práctica clínica en unidades de atención médica correspondiente al semestre durante al menos 2 semanas al final de cada semestre del 5 al 10, siempre con un asesor educativo en el proceso."
+        "8.- ¿CÓMO SON LAS CLASES PRÁCTICAS?",
+        "Las prácticas de simulación a distancia se llevan desde el 1.er semestre, utilizando mesa Anatomage, compatible únicamente con laptop o computadora de escritorio. A partir del 5.º semestre se desarrollan actividades prácticas en dos escenarios distintos: uno mediante la simulación a distancia con la más alta tecnología, como lo desarrollan las mejores universidades del mundo, y además se lleva a cabo práctica clínica en unidades de atención médica correspondiente al semestre durante al menos 2 semanas al final de cada semestre del 5.º al 10.º, siempre con un asesor educativo en el proceso."
     ],
     [
-        "9.- ¿ME TENGO QUE CONECTAR DIARIAMENTE Y CUANTAS HORAS? INTERNADO Y SERVICIO SOCIAL",
-        "Se desarrollan las actividades en un modelo flexible de educación digital, en el que el alumno administra su propio tiempo dándose un rango de una semana para desarrollar las actividades planeadas, en las que el alumno es el gestor de su propio aprendizaje, en el entendido de que se deben atender y realizar la totalidad de las actividades planeadas en plataforma, de igual forma existen actividades prácticas que son obligatorias en las que se diseñan con base en las necesidades grupales a través del tutor del grupo."
+        "9.- ¿ME TENGO QUE CONECTAR DIARIAMENTE Y CUÁNTAS HORAS?",
+        "Se desarrollan las actividades en un modelo flexible de educación digital, en el que el alumno administra su propio tiempo dándose un rango de una semana para desarrollar las actividades planeadas, en las que el alumno es el gestor de su propio aprendizaje, en el entendido de que se deben atender y realizar la totalidad de las actividades planeadas en plataforma. De igual forma, existen actividades prácticas que son obligatorias y que se diseñan con base en las necesidades grupales a través del tutor del grupo. Sin embargo, se solicita que al menos se cumpla con un 66% de asistencia a clases."
     ],
     [
-        "10.- ¿DONDE SE REALIZARÁN?",
-        "En sedes clínicas de primer y segundo nivel acorde a las necesidades de la asignatura, se pretende que se desarrollen en la ciudad de origen las prácticas, sin embargo, el internado y servicio social se desarrolla de forma totalmente presencial buscando dar prioridad al lugar de origen del alumno, pero en el entendido de que es posible acorde a las indicaciones de las instituciones y autoridades de salud que exista movilidad."
+        "10.- ¿DÓNDE SE REALIZARÁN LAS ROTACIONES CLÍNICAS?",
+        "En sedes clínicas de primer y segundo nivel, acorde a las necesidades de la asignatura. Se pretende que se desarrollen en la ciudad de origen las prácticas; sin embargo, el alumno tendrá que adaptarse a las necesidades de la Universidad y a la disponibilidad de campo clínico en el estado de origen.<br /><br />El internado y el servicio social se desarrollan de forma totalmente presencial, buscando dar prioridad al lugar de origen del alumno, pero en el entendido de que es posible, acorde a las indicaciones de las instituciones y autoridades de salud, que exista movilidad."
     ],
     [
         "11.- ¿SE CUBRIRÁN GUARDIAS DURANTE LAS CLASES?",
-        "No, las guardias se desarrollan durante el internado, existen actividades prácticas en los semestres del 5 al 10, durante las dos semanas finales del semestre, pero no en forma de guardias de 24 hrs."
+        "No, las guardias se desarrollan durante el internado. Existen actividades prácticas en los semestres del 5.º al 10.º, durante las dos semanas finales del semestre, pero no en forma de guardias de 24 horas."
     ],
     [
-        "12.- ¿CON QUE INSTITUCIONES DE SALUD SE TIENEN CONVENIOS?",
-        "Se cuentan con convenios generales de la UAD con todas las instituciones de salud públicas IMSS, ISSSTE y SS, además de convenios a lo largo del país con instituciones privadas, sin embargo, los convenios específicos, por ley se desarrollan una vez que se acerquen los ciclos clínicos, y se basan en estos convenios, por lo que no es posible tenerlo en este momento, sin embargo, el paso del convenio general esta adecuado."
+        "12.- ¿CON QUÉ INSTITUCIONES DE SALUD SE TIENEN CONVENIOS?",
+        "Se cuenta con convenios generales de la UAD con todas las instituciones de salud públicas: IMSS, ISSSTE y SS, además de convenios a lo largo del país con instituciones privadas. Sin embargo, los convenios específicos, por ley, se desarrollan una vez que se acerquen los ciclos clínicos y se basan en estos convenios, por lo que no es posible tenerlos en este momento; sin embargo, el paso del convenio general está adecuado."
     ],
     [
-        "13.- ¿SE REALIZAN DE MANERA VIRTUAL?",
-        "Las prácticas son de manera presencial con paciente real, independientemente de la realización de clases y actividades de simulación a distancia si se realizan de manera virtual."
+        "13.- ¿SE REALIZAN DE MANERA VIRTUAL LAS PRÁCTICAS CLÍNICAS?",
+        "Las prácticas son de manera presencial con paciente real, independientemente de la realización de clases y actividades de simulación a distancia, que se realizan de manera virtual."
     ],
     [
-        "14.- ¿COMO SE VINCULAN A LOS CAMPOS CLÍNICOS?",
-        "De la misma forma que cualquier Escuela de Medicina de la Universidad, a través de convenios y docentes tutores para los semestres clínico y de prácticas de internado y servicio social como cualquier Escuela de Medicina."
+        "14.- ¿CÓMO SE VINCULAN A LOS CAMPOS CLÍNICOS?",
+        "De la misma forma que cualquier Escuela de Medicina de la Universidad, a través de convenios y docentes-tutores para los semestres clínicos y de prácticas de internado y servicio social, como cualquier Escuela de Medicina."
     ],
     [
-        "15.- ¿TAMBIÉN ESTA AVALADA POR COMAEM AUNQUE SEA VIRTUAL?",
-        "La Escuela de Medicina UAD Virtual al igual que el resto de Escuela de Medicina de nuestro sistema, se encuentra regida con los máximos procesos de calidad, actualmente la UAD cuenta con la totalidad de Escuelas de Medicina acreditadas (8) y en proceso de acreditación (3), el momento adecuado para iniciar trámites es tercer semestre, por lo cual aún en virtual, aunque se tiene planes de hacerlo, no es posible por la normatividad, tener aún una acreditación."
+        "15.- ¿TAMBIÉN ESTÁ AVALADA POR COMAEM AUNQUE SEA VIRTUAL?",
+        "La Escuela de Medicina UAD Virtual, al igual que el resto de las Escuelas de Medicina de nuestro sistema, se encuentra regida por los máximos procesos de calidad. Actualmente, la UAD cuenta con la totalidad de las Escuelas de Medicina acreditadas (8) y en proceso de acreditación (3). Aunque se tienen planes de hacerlo, no es posible, por la normatividad, contar aún con una acreditación; además, COMAEM aún no cuenta con un instrumento evaluador para nuestra modalidad Mixta-Virtual."
     ],
     [
         "16.- ¿SE CUENTA CON EL DICTAMEN APROBATORIO DE CIFRHS?",
-        "Se cuenta con opinión favorable de CIFRHS Durango, y su respectivo RVOE."
+        "Se cuenta con convenio vigente con CIFRHS Durango y su respectivo RVOE."
     ],
     [
-        "17.- ¿QUIENES SON LOS MAESTROS QUE IMPARTEN LAS CLASES?",
+        "17.- ¿QUIÉNES SON LOS MAESTROS QUE IMPARTEN LAS CLASES?",
         "Contamos con docentes de toda la república con experiencia en su área y en educación médica."
     ],
     [
         "18.- ¿QUÉ TIPO DE REQUERIMIENTOS TECNOLÓGICOS NECESITO PARA INICIAR MIS CLASES?",
-        "Conexión a internet, nivel básico-intermedio de computación y uso de TICS, además de equipo de cómputo y/o dispositivo móvil."
+        "Conexión a internet, nivel básico-intermedio de computación y uso de TIC, además de equipo de cómputo o dispositivo móvil."
     ],
     [
         "19.- ¿SE REALIZA EXAMEN DE ADMISIÓN?",
-        "Por el momento no se cuenta con un examen de admisión, sino que existe un proceso de selección correspondiente a un curso de inducción a la licenciatura."
+        "Por el momento no se cuenta con un examen de admisión, sino que existe un proceso de inducción a la licenciatura."
     ],
     [
         "20.- ¿EXISTE ALGÚN TIPO DE CURSO DE INDUCCIÓN?",
-        "Sí con duración de una semana que se desarrolla de forma virtual."
+        "Sí, con duración de una semana, que se desarrolla de forma virtual y es obligatorio."
     ],
     [
-        "21.- ¿TENGO QUE TENER ALGÚN UNIFORME, AUNQUE SEA VIRTUAL?",
-        "Si, el uniforme se debe utilizar por todos los alumnos en las actividades que se represente a la Escuela."
+        "21.- ¿ES NECESARIO UTILIZAR ALGÚN UNIFORME O CUMPLIR CON UN CÓDIGO DE VESTIMENTA, AUN CUANDO LAS CLASES SEAN EN MODALIDAD VIRTUAL?",
+        "Sí, el uniforme se debe utilizar por todos los alumnos en las actividades en las que se represente a la Escuela, incluidas las clases virtuales y los ciclos clínicos."
     ],
     [
         "22.- ¿TIENE LA MISMA VALIDEZ UNA LICENCIATURA VIRTUAL?",
-        "La misma, y no representa problema para ingresar estudios posteriores como Especialidad, Maestría o Doctorado, así como tampoco para ejercer la medicina en cualquiera de sus escenarios."
+        "Sí, tiene la misma validez y no representa problema para ingresar a estudios posteriores como Especialidad, Maestría o Doctorado, así como tampoco para ejercer la medicina en cualquiera de sus escenarios."
     ],
     [
         "23.- PROCESOS DE TITULACIÓN, ¿SON VIRTUALES O EN ALGÚN MOMENTO ME TENGO QUE PRESENTAR FÍSICAMENTE EN LA INSTITUCIÓN?",
-        "No es necesario, ya que todos tus estudios son en línea y podrás realizar todo proceso académico y/o administrativo por esta vía. Sin embargo, formas parte de la Comunidad Educativa Lobos, por lo que estas invitado a visitarnos en cualquiera de nuestros más de 28 Campus."
+        "El proceso de titulación es mixto: primero es necesario acreditar el examen EGEL de forma virtual; posteriormente, de forma presencial, deberás presentar el examen ECOE en el Hospital Virtual de la ciudad de Durango."
     ],
     [
-        "24.- ¿QUÉ BIBLIOTECAS DIGITALES SE UTILIZAN Y SON GRATUITOS?",
-        "Se cuenta con una biblioteca digital OVID, así como con plataformas de la última tecnología desarrolladas por la Universidad.<br>EXTRANJEROS COMUNICARSE DIRECTAMENTE CON MEDICINA VIRTUAL."
+        "24.- ¿QUÉ BIBLIOTECAS DIGITALES SE UTILIZAN Y SON GRATUITAS?",
+        "Se cuenta con una biblioteca digital OVID, así como con plataformas de la última tecnología desarrolladas por la Universidad.<br /><br />Actualmente no se admiten alumnos que residan en el extranjero."
     ]
 ]
 
